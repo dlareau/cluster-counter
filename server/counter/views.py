4f8a4ab7f567ffd76e89d75c3c3a32288973f5d7
Dillon Lareau <jlareau@jlareau.club.cc.cmu.edu>
2016-02-17 16:12:58 -0500

    now limited to cluster only incrementing

--- server/counter/views.py
+++ server/counter/views.py
@@ -8,14 +8,22 @@ def query(request):
     return HttpResponse(str(counter.count))
 
 def increment(request):
+    client_ip = request.META['REMOTE_ADDR']
     counter = Counter.objects.get(pk=1)
-    counter.count = counter.count+1
-    counter.save()
+    if(len(client_ip) == 13 and client_ip[0:10] == '128.2.100.' and
+       int(client_ip[-3:]) >= 134 and int(client_ip[-3:]) <= 157):
+        counter.count = counter.count+1
+        counter.save()
     return HttpResponse(str(counter.count))
 
 def index(request):
     return increment(request)
 
 def interactive(request):
+    client_ip = request.META['REMOTE_ADDR']
     counter = Counter.objects.get(pk=1)
-    return render(request, "interactive.html", {'count': counter.count})
+    button = False
+    if(len(client_ip) == 13 and client_ip[0:10] == '128.2.100.' and
+       int(client_ip[-3:]) >= 134 and int(client_ip[-3:]) <= 157):
+        button = True
+    return render(request, "interactive.html", {'count': counter.count, 'button': button})
